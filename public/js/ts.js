18c118ea2a3dbf66246ca7ec27933da5856a5393
Tyler J Barnes <b4rn3scode@gmail.com>
2015-09-22 11:25:25 -0700

    setting up assets

---
+++ public/js/ts.js
@@ -0,0 +1,13 @@
+/*
+ * ts
+ * javascript for timesheet application
+ *
+ * @author			Tyler J Barnes
+ * @author			Chris Schaefer
+ * @contact			tbarnes@arbsol.com
+ * @version			1.0
+ */
+
+// strict mode
+'use strict';
+
