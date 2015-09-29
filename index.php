46fbfbaa0183d0833d7994338594591878ed1c05
Chris Schaefer <schaefer007@gmail.com>
2015-09-29 11:06:17 -0400

    nonsense

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // only for production purposes....
 // until the application class and supporting modules are built
+echo "Testing my nonsense";
 include_once("modules/user/views/login.php");
 ?>
