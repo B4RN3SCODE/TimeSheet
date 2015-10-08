e7b610cdf47f68edb3140925b852086fa7d47a10
Tyler J Barnes <b4rn3scode@gmail.com>
2015-10-08 14:55:37 -0700

    get last error msg

--- testdb.php
+++ testdb.php
@@ -3,8 +3,11 @@ include_once("include/config.php");
 include_once("include/DBCon.php");
 $db=new DBCon();
 $db->Link();
-$db->setQueryStmt("SELECT 1");
-$db->Query();
+$db->setQueryStmt("SELECT * FROM Country");
+if(!$db->Query()) {
+	echo $db->GetLastErrorMsg();
+	exit;
+}
 var_dump($db->GetAll());
 exit;
 ?>
