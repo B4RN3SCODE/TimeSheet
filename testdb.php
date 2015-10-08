9386290c57bbd654dfac7295b43455c871c14517
Tyler J Barnes <b4rn3scode@gmail.com>
2015-10-08 14:29:28 -0700

    no message

--- testdb.php
+++ testdb.php
@@ -1,5 +1,6 @@
 <?php
-include('include/DBCon.php');
+include_once("include/config.php");
+include_once("include/DBCon.php");
 $db=new DBCon();
 $db->Link();
 $db->setQueryStmt("SELECT * FROM Country");
