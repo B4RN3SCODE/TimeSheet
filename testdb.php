8a775a676222016f2a4536aca25cd81e72858a19
Tyler J Barnes <b4rn3scode@gmail.com>
2015-10-08 14:54:19 -0700

    select 1

--- testdb.php
+++ testdb.php
@@ -3,7 +3,7 @@ include_once("include/config.php");
 include_once("include/DBCon.php");
 $db=new DBCon();
 $db->Link();
-$db->setQueryStmt("SELECT * FROM Country");
+$db->setQueryStmt("SELECT 1");
 $db->Query();
 var_dump($db->GetAll());
 exit;
