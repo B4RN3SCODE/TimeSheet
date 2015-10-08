d92f3351578787aa4a363031351fa3e389d8b971
Tyler J Barnes <b4rn3scode@gmail.com>
2015-10-08 14:42:49 -0700

    checking object

--- testdb.php
+++ testdb.php
@@ -1,11 +1,10 @@
 <?php
-var_dump(function_exists("mysqli_query"));
 include_once("include/config.php");
 include_once("include/DBCon.php");
 $db=new DBCon();
 $db->Link();
 $db->setQueryStmt("SELECT * FROM Country");
 $db->Query();
-var_dump($db->GetAll());
+var_dump($db);
 exit;
 ?>
