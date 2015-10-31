766db23331a20b2509da03f6dc2ab6ebc984d4a2
Chris Schaefer <schaefer007@gmail.com>
2015-10-31 18:34:32 -0400

    Created project class

---
+++ classes/Project.php
@@ -0,0 +1,134 @@
+<?php
+
+class ProjectArray extends ArrayClass {
+    protected $db;
+
+    function __construct(){
+        parent::__construct("Project");
+		if(!isset($GLOBALS["APP"]["INSTANCE"])) {
+			$GLOBALS["APP"]["INSTANCE"]->_dbAdapter = new DBCon();
+			$GLOBALS["APP"]["INSTANCE"]->_dbAdapter->Link();
+		}
+		$this->db = $GLOBALS["APP"]["INSTANCE"]->_dbAdapter;
+        $this->db->setTBL(self::getClass()); // ArrayClass function
+    }
+
+    function load() {
+        $strSQL = $this->db->SStatement(array(), self::getClass());
+        $this->db->SetQueryStmt($strSQL);
+        if($this->db->Query()) {
+            foreach ($this->db->GetAll() as $row) {
+                $this->_arrObjects[$row["id"]] = new User();
+                $this->_arrObjects[$row["id"]]->setVarsFromRow($row);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class User extends BaseDB {
+    protected $_id;
+    protected $_UserId;
+    protected $_ClientId;
+    protected $_Title;
+    protected $_Description;
+    protected $_DateCreated;
+    protected $_Rate;
+    protected $columns = array("id","Email","FirstName","LastName","AccountType","Password",
+        "DateAdded","LastModified","Phone","Active","Restriction","Online");
+    protected $db;
+
+    public function getId() { return $this->_id; }
+    public function getUserId() { return $this->_UserId; }
+    public function getClientId() { return $this->_ClientId; }
+    public function getTitle() { return $this->_Title; }
+    public function getDescription() { return $this->_Description; }
+    public function getDateCreated() { return $this->_DateCreated; }
+    public function getRate() { return $this->_Rate; }
+
+    public function setId($value) { $this->_id = $value; }
+    public function setUserId($value) { $this->_UserId = $value; }
+    public function setClientId($value) { $this->_ClientId = $value; }
+    public function setTitle($value) { $this->_Title = $value; }
+    public function setDescription($value) { $this->_Description = $value; }
+    public function setDateCreated($value) { $this->_DateCreated = $value; }
+    public function setRate($value) { $this->_Rate = $value; }
+
+    public function __construct($id=null) {
+        $this->db = $GLOBALS["APP"]["INSTANCE"]->_dbAdapter;
+        $this->db->setTBL(get_class($this));
+        if($id) {
+            $this->load($id);
+        }
+    }
+
+    public function delete() {
+        if($this->_id) {
+            $strSQL = "DELETE FROM " . DB_NAME . "." . get_class($this) . "
+				WHERE id = $this->_id";
+            $this->db->setQueryStmt($strSQL);
+            return $this->db->Query();
+        }
+    }
+
+    private function insert() {
+        $strSQL = $this->db->IStatement(get_class($this),self::prepare_data());
+        $this->db->setQueryStmt($strSQL);
+        if($this->db->Query()) {
+            $this->_id = $this->db->GetLastInsertedId();
+            return $this->_id;
+        } else {
+            return false;
+        }
+    }
+
+    public function load($id)
+    {
+        if (!$id) return false;
+        $strSQL = $this->db->SStatement(array(), get_class($this), array("id" => strval($id)));
+        $this->db->setQueryStmt($strSQL);
+        if ($this->db->Query()) {
+            $this->setVarsFromRow($this->db->getRow());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function LoadByTitle($Title = null) {
+        if(!$Title) return false;
+        $strSQL = $this->db->SStatement(array(), get_class($this), array("Title" => array(0 => $Title)));
+        $this->db->setQueryStmt($strSQL);
+        if ($this->db->Query()) {
+            $this->setVarsFromRow($this->db->GetRow());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function save() {
+        $this->setLastModified(base::now());
+        if($this->_id) {
+            return self::update();
+        } else {
+            $this->setDateAdded(base::now());
+            return self::insert();
+        }
+    }
+
+    private function update() {
+        $strSQL = $this->db->UStatement(self::prepare_data(),get_class($this),array("id" => array(0 => $this->getId())));
+        $this->db->setQueryStmt($strSQL);
+        if($this->db->Query())
+            return ($this->db->GetAffectedRows() > 0);
+        return false;
+    }
+
+	public function setDB(DBCon $db) {
+		$this->db = $db;
+	}
+}
+?>
--- testdb.php
+++ testdb.php
@@ -12,4 +12,23 @@ echo "<pre>";
 print_r($db->GetAll());
 echo "</pre>";
 exit;
+
+
+$columns = array("id","UserId","ClientId","Title","Description","DateCreated",
+    "Rate");
+foreach($columns as $column) {
+    echo "protected \$_$column;<br />";
+}
+echo "<br />";
+foreach($columns as $column) {
+    echo "public function get";
+    echo ucwords($column);
+    echo "() { return \$this->_$column; }<br />";
+}
+echo "<br />";
+foreach($columns as $column) {
+    echo "public function set";
+    echo ucwords($column);
+    echo "(\$value) { \$this->_$column = \$value; }<br />";
+}
 ?>
