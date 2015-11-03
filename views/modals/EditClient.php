a5ab756c4e5deaec2e9ffde2b7985e912bf2e712
Chris Schaefer <schaefer007@gmail.com>
2015-11-02 21:57:19 -0500

    Update Verb

--- views/modals/EditClient.php
+++ views/modals/EditClient.php
@@ -3,7 +3,7 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title">Update Client</h4>
+				<h4 class="modal-title">Edit Client</h4>
 			</div>
 			<form name="editclient" action="/TimeSheet/Admin/UpdateClient" method="post"><!--onsubmit="return false;">-->
 				<input type="hidden" name="id" value="" />
