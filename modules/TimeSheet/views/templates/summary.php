7c9e68b14c616676516423815e24ad7eab977c3c
Chris Schaefer <schaefer007@gmail.com>
2015-11-06 20:21:31 -0500

    Removed useless code and a todo notification

--- modules/TimeSheet/views/templates/summary.php
+++ modules/TimeSheet/views/templates/summary.php
@@ -1,30 +1,3 @@
-<?php
-//print_r($TPLDATA["TimeSheets"]);
-//$TPLDATA["TimeSheets"] = array();
-//$end_date = new DateTime($_SESSION["CurrentBillingPeriod"]["StartDate"]);
-//$start_date = clone($end_date);
-//date_sub($start_date,new DateInterval("P16W"));
-//
-//$DatePeriod = new DatePeriod($start_date, new DateInterval('P2W'), $end_date);
-//foreach($DatePeriod as $date) {
-//	$end_date = clone($date);
-//	$end_date = $end_date->modify("+13 days");
-//	$TimeSheetArray[] = array("Start"=>$date->format("m/d/Y"),
-//		"End"=>$end_date->format("m/d/Y"),
-//		"Total"=>rand(0,8),
-//		"Billable"=>rand(0,8));
-//}
-//$start_date = new DateTime($_SESSION["CurrentBillingPeriod"]["StartDate"]);
-//$end_date = new DateTime($_SESSION["CurrentBillingPeriod"]["EndDate"]);
-//$TimeSheetArray[] = array("Start"=>$start_date->format("m/d/Y"),
-//	"End"=>$end_date->format("m/d/Y"),
-//	"Total"=>rand(0,8),
-//	"Billable"=>rand(0,8));
-//$TimeSheetArray = array_reverse($TimeSheetArray);
-?>
-<div class="alert alert-danger">
-	Get submit, unsubmit buttons working
-</div>
 <div id="TimeSheetSummary" class="col-md-12">
 	<div class="panel panel-default">
 		<div class="panel-heading">
