f2dd779760701165866da9968ba7cb461fdac302
Tyler J Barnes <b4rn3scode@gmail.com>
2015-12-04 09:23:00 -0800

    no message

--- sc.php
+++ sc.php
@@ -43,7 +43,7 @@
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="/User/Login">Login</a></li>
+						<li><a href="#">Login</a></li>
 					</ul>
 				</div>
 			</div><!-- /.container-fluid -->
