c8e0b19098cb16918bd521791cee8460a67d4ba8
Tyler J Barnes <b4rn3scode@gmail.com>
2015-12-04 09:09:28 -0800

    added resources so htaccess rewrite didnt throw errors

---
+++ sc/sc.js
@@ -0,0 +1,21 @@
+/*
+ * SC
+ * Snake Charmer Application
+ *
+ * @author		Tyler J Barnes
+ * @contact		b4rn3scode@gmail.com
+ * @version		1.0
+ * @doc			www.barnescode.com/sc/README.txt
+ */
+
+// strict mode
+'use strict';
+
+/*
+ * main object
+ * @return void
+ */
+var SC = function() {
+
+};
+// END SC
