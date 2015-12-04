e5bb90a4cb71ff33507e6dafb99b5a26b496c727
Tyler J Barnes <b4rn3scode@gmail.com>
2015-12-04 09:02:00 -0800

    snake charmer testing stuff

---
+++ sc.php
@@ -0,0 +1,83 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+
+		<title>Snake Charmer Testing Page</title>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700' rel='stylesheet' type='text/css'>
+		<link href="//www.barnescode.com/sc/bootstrap.css" rel="stylesheet">
+		<link href="//www.barnescode.com/public/style/style.css" rel="stylesheet">
+		<link href="//www.barnescode.com/public/style/main.css" rel="stylesheet">
+
+
+		<!--		SNAKE CHARMER CSS AND JS		-->
+		<link href="//www.barnescode.com/sc/style.css" rel="stylesheet">
+		<script src="//maxcdn.bootstrapcdn.com/sc/sc.js"></script>
+		<!--		SNAKE CHARMER CSS AND JS		-->
+
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/sc/bootstrap.min.js"></script>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script src="//www.barnescode.com/public/js/ts.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Snake Charmer Test</a>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="/User/Login">Login</a></li>
+					</ul>
+				</div>
+			</div><!-- /.container-fluid -->
+		</nav>
+		<div class="container">
+			<div class="row">
+				<h4 class="text-center sc-hover-event-trigger">Test Login Form</h4>
+				<div class="col-md-offset-4 col-md-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title sc-hover-event-trigger">Add events to the following</h3>
+						</div>
+						<div class="panel-body">
+							<form action="#" method="POST">
+								<label for="inputEmail" class="sr-only">Email address</label>
+								<input type="email" name="email" id="txtEmail" class="form-control input-lg" placeholder="Email address" required autofocus>
+								<label for="inputPassword" class="sr-only">Password</label>
+								<input type="password" name="password" id="txtPassword" class="form-control input-lg" placeholder="Password" required>
+								<div class="checkbox">
+									<label><input name="rememberme" id="chkRememberMe" type="checkbox" value="1">Remember me</label>
+								</div>
+								<button id="btnSubmit" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--		SNAKE CHARMER SECTION BEGIN		-->
+		<div id="SCWidget" class="">
+			<!-- SNAKE CHARMER SHIT -->
+		</div>
+		<!--		SNAKE CHARMER SECTION END		-->
+
+	</body>
+</html>
