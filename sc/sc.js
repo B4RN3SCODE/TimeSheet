f613caeb0050bd9e5efb630376f59f207b8f8fd9
Tyler J Barnes <b4rn3scode@gmail.com>
2015-12-07 15:29:40 -0800

    added more functions to framework

--- sc/sc.js
+++ sc/sc.js
@@ -14,16 +14,22 @@
 /*
  * main object
  *
+ * @param autoRender boolean should the widget render automatically
  * @param config object with config values
  * @return void
  */
-var SC = function(config) {
+var SC = function(autoRender,config) {
 
+	// autoRender
+	this._autoRender = (autoRender===true);
 	// config
 	this._config = (!!config && (typeof config).toLowerCase() == 'object') ? config : undefined;
 	// jQuery
-	this._$ = ('undefined'==typeof jQuery || 'undefined' == $) ? undefined : $;
-
+	this._$ = ('undefined'==typeof jQuery) ? -1 : jQuery;
+	// theme data
+	this._themeData = {};
+	// notification data
+	this._notificationData = {};
 
 
 	/*
@@ -33,11 +39,28 @@ var SC = function(config) {
 	 * @return false if failure
 	 */
 	this.ini = function() {
+
 		// make sure jQuery is loaded
-		if(typeof this._$ == undefined) {
+		if(this._$ == -1 || typeof this._$ == 'undefined') {
 			console.error('Snake Charmer Relies on jQuery. Please include jQuery library on your web page');
 			return false;
 		}
+
+		// mandatory config vars
+		var conf = ['pageUri','license','themeId'];
+		// make sure all config is correct
+		for(var i in conf) {
+			if(!(conf[i] in this._config)) {
+				console.warn('Invalid configuration detected. Please see documentation to include all config values for Snake Charmer. Using default config');
+				this._config = undefined;
+				break;
+			}
+		}
+		// set default config if undefined
+		if(typeof this._config == 'undefined') {
+			this._config = this.getDefaultConfig();
+		}
+
 
 
 	};
@@ -75,6 +98,10 @@ var SC = function(config) {
 	 */
 	this.ajax = function(url, data, type, err, succ) {
 		var u = (!!url && url.replace(/\ /g,'').length > 0) ? url : undefined;
+		if('undefined'==typeof u) {
+			console.error('Invalid ajax url');
+			return false;
+		}
 		var d = data;
 		var t = (type.toUpperCase() == 'POST' || type.toUpperCase() == 'GET') ? type.toUpperCase() : 'POST';
 		var e = err;
@@ -89,6 +116,30 @@ var SC = function(config) {
 		this._$.ajax({
 			url: u, data: d, type: t, error: e,	success: function(d) { s(d); }
 		});
+
+		return true;
+	};
+
+
+
+	/*
+	 * getThemeData
+	 * gets the theme data for the widget
+	 *
+	 * @return void
+	 */
+	this.getThemeData = function() {
+	};
+
+
+
+	/*
+	 * getNotifData
+	 * gets notification, event, and page data
+	 *
+	 * @reutn void
+	 */
+	this.getNotifData = function() {
 	};
 };
 // END SC
