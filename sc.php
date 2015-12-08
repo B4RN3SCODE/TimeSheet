d7af004ad4a3868cead69a85d81ad295b50fd7ef
Tyler J Barnes <b4rn3scode@gmail.com>
2015-12-08 13:09:38 -0800

    updated head tags

--- sc.php
+++ sc.php
@@ -4,8 +4,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
 
 		<title>Snake Charmer Testing Page</title>
-		<link href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 		<link href="//www.barnescode.com/sc/css/bootstrap.min.css" rel="stylesheet">
 		<link href="//www.barnescode.com/public/style/style.css" rel="stylesheet">
 		<link href="//www.barnescode.com/public/style/main.css" rel="stylesheet">
