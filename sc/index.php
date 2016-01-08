2bf9e4ba28600c927cded2e16968f74aea41c305
Tyler J Barnes <b4rn3scode@gmail.com>
2016-01-08 13:11:18 -0800

    a

--- sc/index.php
+++ sc/index.php
@@ -1,6 +1,9 @@
 <html>
 <head>
 	<title>Fuck Off</title>
+	<link href="//content.blogninja.com/cts/assets/smv2/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="//content.blogninja.com/cts/assets/smv2/js/bootstrap.min.js"></script>
 	<!--		SNAKE CHARMER JS		-->
 	<script type="text/javascript" id="SCJS" src="//www.conversionvoodoo.com/sc/js/sc.js?license=xkH4edko9Kl3&themeId=1"></script>
 	<!--		END SNAKE CHARMER JS		-->
