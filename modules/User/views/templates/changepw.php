c08895ad5b172d94f6b2f83f4ee6241ed9c6b7ce
Chris Schaefer <schaefer007@gmail.com>
2016-01-29 21:45:05 -0500

    Added generate password to the change password form.

--- modules/User/views/templates/changepw.php
+++ modules/User/views/templates/changepw.php
@@ -8,15 +8,20 @@
         <input type="hidden" name="form" value="change-password">
         <div class="form-group">
           <label for="old-pw">Old Password</label>
-          <input type="password" class="form-control" name="old-pw" autofocus>
+          <input type="text" class="form-control" name="old-pw" autofocus>
         </div>
         <div class="form-group">
           <label for="new-pw">New Password</label>
-          <input type="password" class="form-control" name="new-pw">
+          <div class="input-group">
+            <input type="text" class="form-control" name="new-pw">
+            <span class="input-group-btn">
+              <button data-task="PWGen" data-target="[name='new-pw'],[name='cfm-pw']" class="btn btn-primary">Generate</button>
+            </span>
+          </div>
         </div>
         <div class="form-group">
           <label for="cfm-pw">Confirm Password</label>
-          <input type="password" class="form-control" name="cfm-pw">
+          <input type="text" class="form-control" name="cfm-pw">
         </div>
         <input type="submit" class="btn btn-secondary btn-block" value="Change Password" />
       </form>
